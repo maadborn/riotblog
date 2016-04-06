1f5282a0b9f8c86758917936e7c8daaf3e614ac6
maadborn <mats.adborn@gmail.com>
2016-04-06 21:04:06 +0200

    sass build with tags, watch, browserify into a single app.css

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,9 @@ var riotify     = require('riotify');
 var changed     = require('gulp-changed');
 var del         = require('del');
 var sass		= require('gulp-sass');
+var merge 		= require('merge-stream');
+var concat		= require('gulp-concat');
+var autoprefix 	= require('gulp-autoprefixer');
 
 // Paths object
 var paths = {
@@ -24,10 +27,11 @@ var paths = {
         sourceMapFilename: 'bundle.js.map'
     },
     styles: {
-        src: './src/styles/*.css',
+        src: ['./src/styles/*.css'],
         dest: './dist/styles',
         distGlob: './dist/**/*.css',
-		sassSrc: ['./src/styles/**/*.scss', './src/tags/**/*.scss']
+		sassSrc: ['./src/styles/**/*.scss', './src/tags/**/*.scss'],
+		mergedStyleFile: 'app.css'
     },
     html: {
         src: './src/*.html',
@@ -38,11 +42,18 @@ var paths = {
 	browsersyncBaseDir: './dist'
 };
 
+var sassOptions = {};
+var autoprefixerOptions = {};
+var riotifyOptions = { 
+	ext: 'tag.html', 
+	type: 'babel' 
+};
+
 // Prepare scripts for package/distribution/browser
 function compile(watch) {
     var bundler = browserify(paths.scripts.bundleEntry, { debug: true })
         .transform(babelify)
-        .transform(riotify, { ext: 'tag.html', type: 'babel' })
+        .transform(riotify, riotifyOptions)
 		;
 	
     var watcher = watch ? watchify(bundler) : '';
@@ -60,7 +71,7 @@ function compile(watch) {
     function rebundle() {
         return bundler.bundle()
             .on('error', function(err) { 
-				gutil.log('Bundle error: ' /* + err.message + '\n'*/ + err);  
+				gutil.log('Bundle error: ', err);  
                 browsersync.notify('Browserify error!');
                 this.emit('end'); 
             })
@@ -83,19 +94,37 @@ gulp.task('clean', function() {
     del(paths.clean);
 });
 
-// Styles transformation task TODO
-gulp.task('styles', function() {
+/*
+DEPRECATED, use 'styles' for a complete sass+css build
+gulp.task('stylesOld', function() {
     return gulp.src(paths.styles.src)
         //concat, autoprefix, minify
         .pipe(gulp.dest(paths.styles.dest));
 });
+*/
 
+gulp.task('styles', function() {
+	var cssStream = gulp.src(paths.styles.src);
+	
+	var sassStream = gulp.src(paths.styles.sassSrc)
+		.pipe(sass(sassOptions).on('error', sass.logError));
+	
+    return merge(cssStream, sassStream)
+		.pipe(autoprefix(autoprefixerOptions))
+		.pipe(concat(paths.styles.mergedStyleFile))
+        .pipe(gulp.dest(paths.styles.dest))
+		.pipe(browsersync.stream());
+});
+
+/*
+DEPRECATED, use 'styles' for a complete sass+css build
 gulp.task('sass', function() {
 	return gulp.src(paths.styles.sassSrc)
-		.pipe(sass().on('error', sass.logError))
+		.pipe(sass(sassOptions).on('error', sass.logError))
 		.pipe(gulp.dest(paths.styles.dest))
 		.pipe(browsersync.stream());
 });
+*/
 
 // Html move task
 gulp.task('html', function() {
@@ -111,17 +140,20 @@ gulp.task('build', ['styles', 'html'], function () {
 });
 
 // Watch sources for development
-gulp.task('watch', ['styles', 'html', 'sass'], function () {  
+gulp.task('watch', ['styles', 'html'], function () {  
+    var stylesSrc = paths.styles.src.concat(paths.styles.sassSrc);
+	
+	gulp.watch(stylesSrc, ['styles']);
     gulp.watch(paths.html.src, ['html']);
-    gulp.watch(paths.styles.src, ['styles']);
-	gulp.watch(paths.styles.sassSrc, ['sass']);
-    return watch(); 
+    
+	return watch(); 
 });
 
 // browsersync task 
 gulp.task('browsersync', ['watch'], function () {
     gutil.log('Starting browsersync...');
-    browsersync.init({
+    
+	browsersync.init({
         files: [paths.html.distGlob, paths.styles.distGlob],
         server: { 
             baseDir: paths.browsersyncBaseDir
